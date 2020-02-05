0d5b79378d1a9baa11a2cde75de858e0124ed906
kevinzhu2019 <kevinzhu.zyk@gmail.com>
2020-02-05 23:15:22 +0000

    Completed basic http authentication

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: "Jungle", password: "book", except: [:index, :show]
+
   def index
     @products = Product.order(id: :desc).all
   end
