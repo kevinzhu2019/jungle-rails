431c504c2c5632ec0770c5e20406b3a0adf4f352
kevinzhu2019 <kevinzhu.zyk@gmail.com>
2020-02-05 23:50:23 +0000

    Completed Feature: Admin Dashboard - Counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,9 @@
 class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with name: "Jungle", password: "book", except: [:index, :show]
-  
+
   def show
+    @product_amount = Product.count
+    @category_amount = Category.count
   end
 end
